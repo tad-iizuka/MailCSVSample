734d2b685a530cf1508970cb8d576d7aa6b6a664
tad-iizuka <tad.iizuka@gmail.com>
2016-09-28 08:16:37 +0900

    Initial commit

--- MailCSVSample/ViewController.swift
+++ MailCSVSample/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
+
+	@IBOutlet weak var SendMail: UIButton!
+	@IBAction func SendMail(_ sender: AnyObject) {
+
+		// Make a CSV object
+		var fileObject = String()
+		let formatter = DateFormatter()
+		formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+		for _ in 1...10 {
+			fileObject.append(String(format:"%@\n", formatter.string(from: NSDate() as Date)))
+		}
+
+		// Make a mail body
+        if MFMailComposeViewController.canSendMail() == false {
+            print(String(format: "Error %@: %d",#file, #line))
+            return
+        }
+
+        let mailCompose = MFMailComposeViewController()
+//		let to = [""]
+//		let cc = [""]
+//		let bcc = [""]
+
+        mailCompose.mailComposeDelegate = self
+        mailCompose.setSubject("Attached CSV file")
+//		mailCompose.setToRecipients(to)
+//		mailCompose.setCcRecipients(cc)
+//		mailCompose.setCcRecipients(cc)
+        mailCompose.setMessageBody("Please make sure.", isHTML: false)
+
+		let data = fileObject.data(using: String.Encoding.utf8)
+		let fName = String(format:"%@\n", formatter.string(from: NSDate() as Date)) + ".csv"
+		mailCompose.addAttachmentData(data!, mimeType: "text/csv", fileName: fName)
+
+        self.present(mailCompose, animated: true, completion: nil)
+
+	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+
+		self.SendMail.layer.borderColor = UIColor.blue.cgColor
+		self.SendMail.layer.borderWidth = 1.0
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
 
+	func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
 
+		if (error != nil) {
+			print(String(format: "Error %@: %d",#file, #line))
+		}
+
+		switch result {
+			case MFMailComposeResult.cancelled: break
+			case MFMailComposeResult.saved: break
+			case MFMailComposeResult.sent: break
+			case MFMailComposeResult.failed: break
+		}
+		self.dismiss(animated: true, completion: nil)
+	}
 }
 
